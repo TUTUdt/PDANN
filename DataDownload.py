684aceb1500d59bef4553d6d0d5f679666ec7d0a
TUTUdt <154779077+TUTUdt@users.noreply.github.com>
2025-03-18 20:40:34 +0800

    Update DataDownload.py

--- DataDownload.py
+++ DataDownload.py
@@ -1,45 +1,3 @@
-# import requests
-# from tqdm import tqdm
-#
-# def download(url: str, fname: str):
-#     # 用流stream的方式获取url的数据
-#     resp = requests.get(url, stream=True)
-#     # 拿到文件的长度，并把total初始化为0
-#     total = int(resp.headers.get('content-length', 0))
-#     # 打开当前目录的fname文件(名字你来传入)
-#     # 初始化tqdm，传入总数，文件名等数据，接着就是写入，更新等操作了
-#     with open(fname, 'wb') as file, tqdm(
-#         desc=fname,
-#         total=total,
-#         unit='iB',
-#         unit_scale=True,
-#         unit_divisor=1024,
-#     ) as bar:
-#         for data in resp.iter_content(chunk_size=1024):
-#             size = file.write(data)
-#             bar.update(size)
-#
-#
-# monthday = [0,31,28,31,30,31,30,31,31,30,31,30,31]
-#
-# for year in range(2018,2023):
-#     for month in range(1,13):
-#         url1 = r'http://data-cbr.csiro.au/thredds/ncss/catch_all/CMAR_CAWCR-Wave_archive/CAWCR_Wave_Hindcast_1979-2010/gridded/ww3.aus_4m.'\
-#                + str(year) + str(month).zfill(2) + \
-#                r'.nc?var=U10&var=V10&var=dir&var=hs&var=t&var=t02&var=tm0m1&north=0&west=135&east=170&south=-50.0000&disableProjSubset=on&horizStride=1&time_start='\
-#                + str(year) + '-' + str(month).zfill(2) + r'-01T00%3A00%3A00Z&time_end=' + str(year) + '-' + str(month).zfill(2) + '-' + str(monthday[month]) + \
-#                r'T23%3A00%3A00Z&timeStride=1&addLatLon=true&accept=netcdf4'
-#         download(url1, str(year)+str(month).zfill(2)+'hiRes.nc4')
-#
-#         url2 = r'https://data-cbr.csiro.au/thredds/ncss/catch_all/CMAR_CAWCR-Wave_archive/CAWCR_Wave_Hindcast_1979-2010/gridded/ww3.glob_24m.'\
-#                + str(year) + str(month).zfill(2) + \
-#                r'.nc?var=U10&var=V10&var=dir&var=hs&var=t02&var=tm0m1&north=0&west=135&east=170&south=-50&disableProjSubset=on&horizStride=1&&time_start='\
-#                + str(year) + '-' + str(month).zfill(2) + r'-01T00%3A00%3A00Z&time_end=' + str(year) + '-' + str(month).zfill(2) + '-' + str(monthday[month]) + \
-#                r'T23%3A00%3A00Z&timeStride=1&accept=netcdf'
-#         print(url2)
-#         download(url2, str(year)+str(month).zfill(2)+'lowRes.nc4')
-
-
 import requests
 from tqdm import tqdm
 import time
@@ -76,8 +34,15 @@ def download(url: str, fname: str):
 
 monthday = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 
-for year in range(2023,2025):
+for year in range(2000,2010):
     for month in range(1,13):
+        url1 = r'https://data-cbr.csiro.au/thredds/ncss/catch_all/CMAR_CAWCR-Wave_archive/CAWCR_Wave_Hindcast_aggregate/gridded/ww3.aus_4m.'\
+               + str(year) + str(month).zfill(2) + \
+               r'.nc?var=U10&var=V10&var=dir&var=hs&var=t&var=t02&var=tm0m1&north=0&west=135&east=170&south=-50.0000&disableProjSubset=on&horizStride=1&time_start='\
+               + str(year) + '-' + str(month).zfill(2) + r'-01T00%3A00%3A00Z&time_end=' + str(year) + '-' + str(month).zfill(2) + '-' + str(monthday[month]) + \
+               r'T23%3A00%3A00Z&timeStride=1&addLatLon=true&accept=netcdf4'
+        download(url1, str(year)+str(month).zfill(2)+'hiRes.nc4')
+        
         url2 = r'https://data-cbr.csiro.au/thredds/ncss/catch_all/CMAR_CAWCR-Wave_archive/CAWCR_Wave_Hindcast_aggregate/gridded/ww3.glob_24m.'\
                + str(year) + str(month).zfill(2) + \
                r'.nc?var=dir&var=hs&var=t02&var=t0m1&var=uwnd&var=vwnd&&north=-35&west=137&east=155&south=-45&disableProjSubset=on&horizStride=1&time_start='\
